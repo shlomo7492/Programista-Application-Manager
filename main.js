442c654cbac215a6fc941ea9aac8bcd1e09bc469
shlomo7492 <shlomo7492@yahoo.com>
2018-10-23 19:30:06 +0300

    CRUD -> CREATE implemented

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var addNew = '<form method="post" action="jstest.html?navigate=listall" id="createForm">'+
+var addNew = '<form method="post" action="index.html?navigate=listall" id="createForm">'+
 			'<div class="form_container form-group" >'+
 			'<!--To use RegEx in pattern attribute to so I can validate the input data -->'+
 				'<h2>Application Form(Add New Student)</h2>'+
 				'<strong>Name:</strong> <input name="name" id="name" type="text" class="form-control" placeholder="Student Full name" size="50" required><br><br>'+
-				'<strong>E-mail:</strong> <input type="text" name="email" class="form-control" size="50" placeholder="Student e-mail" autocomplete="off" required><br><br>'+
-				'<strong>Age:</strong> <input type="number"  name="age" class="form-control" min="16"  required><br><br>'+
-				'<strong>Phone Number:</strong> <input name="phone" type="text" class="form-control" placeholder="Student phone number" size="42" required><br><br>'+
+				'<strong>E-mail:</strong> <input type="text" name="email" id="email" class="form-control" size="50" placeholder="Student e-mail" autocomplete="off" required><br><br>'+
+				'<strong>Age:</strong> <input type="number"  name="age" id="age" class="form-control" min="16"  required><br><br>'+
+				'<strong>Phone Number:</strong> <input name="phone" id="phone" type="text" class="form-control" placeholder="Student phone number" size="42" required><br><br>'+
 				'<strong>Preferred Way of Communication:</strong><br>'+
 				'<input type="radio" name="contactby" value="email" checked> E-mail<br>'+
 				'<input type="radio" name="contactby" value="phone"> Phone<br><br>'+
 				'<strong>English Level: </strong>'+
-				'<select name="englishlevel" class="form-control" required>'+
+				'<select name="englishlevel" id="englishlevel" class="form-control" required>'+
 				'	<option value="beginner">Beginner</option>'+
 				'	<option value="elementary">Elementary(A1)</option>'+
 				'	<option value="preintermediate">Pre-intermediate(A2)</option>'+
@@ -22,15 +22,15 @@ var addNew = '<form method="post" action="jstest.html?navigate=listall" id="crea
 				'	<option value="veryadvanced">Very Advanced(C2)</option>'+
 				'</select><br><br>'+
 				'<strong>Available to Start:</strong>'+
-				'<input type="date" name="startdate" min="" required><br><br>'+
+				'<input type="date" name="startdate"  id="startdate" min="" required><br><br>'+
 				'<strong>Technical Skills and Courses:</strong> <br>'+
-				'<textarea name="skillsandcourses" class="form-control" rows="10"  cols="60" >'+
+				'<textarea name="skillsandcourses" id="skillsandcourses" class="form-control" rows="10"  cols="60" >'+
 				'</textarea><br><br>'+
 				'<strong>Short Personal Presentation:</strong><br>(e.g. reason for joining the program)<br>'+
-				'<textarea name="selfpresentation" class="form-control" rows="10" cols="60"  placeholder="Please write tell us more about yourself and your reasons to join the program?">'+
+				'<textarea name="selfpresentation" id="selfpresentation" class="form-control" rows="10" cols="60"  placeholder="Please write tell us more about yourself and your reasons to join the program?">'+
 				'</textarea><br><br>'+
-				'<input type="checkbox" name="homestudy" value="true">Study from home<br><br>'+
-				'<button id="submit" >Add</button>'+
+				'<input type="checkbox" name="homestudy" id="homestudy" value="true">Study from home<br><br>'+
+				'<button id="submit" onclick="AddNewStudent()" >Add</button>'+
 			'</div>'+
 		'</form>';
 /*Provides simple navigation trough application's functionality 
@@ -61,8 +61,7 @@ function AddNewStudent(){
 	var startdate = document.getElementById('startdate').value;
 	var skillsandcourses = document.getElementById('skillsandcourses').value;
 	var selfpresentation = document.getElementById('selfpresentation').value;	
-	var homestudy = document.getElementById('homestudy').value;	
-	
+	var homestudy = document.getElementById('homestudy').value;
 	var student = {
 		"name":name,
 		"email":email,
@@ -73,7 +72,7 @@ function AddNewStudent(){
 		"startdate":startdate,
 		"skillsandcourses":skillsandcourses,
 		"selfpresentation":selfpresentation,
-		"homestudy":homestudy};
+		"homestudy":homestudy}; 
 	if(localStorage.savedStudents==null){
 			var students = [];
 		}
@@ -82,6 +81,7 @@ function AddNewStudent(){
 		}
 		students.push(student);
 		localStorage.setItem('savedStudents', JSON.stringify(students));
+		
 } 
 /*Reading the data in localStorage 'savedStudents' 
 and displays it in table-like format*/
