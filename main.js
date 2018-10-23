5bc4b666903d9403b3b75d9e8be19d038e32bc67
shlomo7492 <shlomo7492@yahoo.com>
2018-10-23 18:55:50 +0300

    I Found my bug and again I return to single page app

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var addNew = '<form method="post" action="index.html" id="createForm">'+
+var addNew = '<form method="post" action="jstest.html?navigate=listall" id="createForm">'+
 			'<div class="form_container form-group" >'+
 			'<!--To use RegEx in pattern attribute to so I can validate the input data -->'+
 				'<h2>Application Form(Add New Student)</h2>'+
-				'<strong>Name:</strong> <input name="name" type="text" class="form-control" placeholder="Student Full name" size="50" required><br><br>'+
+				'<strong>Name:</strong> <input name="name" id="name" type="text" class="form-control" placeholder="Student Full name" size="50" required><br><br>'+
 				'<strong>E-mail:</strong> <input type="text" name="email" class="form-control" size="50" placeholder="Student e-mail" autocomplete="off" required><br><br>'+
 				'<strong>Age:</strong> <input type="number"  name="age" class="form-control" min="16"  required><br><br>'+
 				'<strong>Phone Number:</strong> <input name="phone" type="text" class="form-control" placeholder="Student phone number" size="42" required><br><br>'+
@@ -30,14 +30,71 @@ var addNew = '<form method="post" action="index.html" id="createForm">'+
 				'<textarea name="selfpresentation" class="form-control" rows="10" cols="60"  placeholder="Please write tell us more about yourself and your reasons to join the program?">'+
 				'</textarea><br><br>'+
 				'<input type="checkbox" name="homestudy" value="true">Study from home<br><br>'+
-				'<input type="hidden" name="navigation" value="create">'+
-				'<input type="submit" name="" value="Submit">'+
-
+				'<button id="submit" >Add</button>'+
 			'</div>'+
 		'</form>';
+/*Provides simple navigation trough application's functionality 
+supplying different page content by using url query parameters*/
 function printContent(navVar){
 	if (navVar == 'add'){
 		document.getElementById("content").innerHTML = addNew;
 		document.getElementById("title").innerHTML += '::Add New student';
 	}
+	else if(navVar == 'listall'){
+		listAllStudents();
+	}
+	
 }
+
+	
+/*Getting fields values from Add New Student form and 
+add them to the localStorage*/
+function AddNewStudent(){
+	//Initialize students array to be in correct JSON format
+	
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var age = document.getElementById('age').value;
+	var phone = document.getElementById('phone').value;
+	var contactBy = $('input:radio[name=contactby]:checked').val();
+	var englishlevel = document.getElementById('englishlevel').value;
+	var startdate = document.getElementById('startdate').value;
+	var skillsandcourses = document.getElementById('skillsandcourses').value;
+	var selfpresentation = document.getElementById('selfpresentation').value;	
+	var homestudy = document.getElementById('homestudy').value;	
+	
+	var student = {
+		"name":name,
+		"email":email,
+		"age":age,
+		"phone":phone,
+		"contactBy":contactBy,
+		"englishlevel":englishlevel,
+		"startdate":startdate,
+		"skillsandcourses":skillsandcourses,
+		"selfpresentation":selfpresentation,
+		"homestudy":homestudy};
+	if(localStorage.savedStudents==null){
+			var students = [];
+		}
+		else{
+			var students=JSON.parse(localStorage.getItem('savedStudents'));
+		}
+		students.push(student);
+		localStorage.setItem('savedStudents', JSON.stringify(students));
+} 
+/*Reading the data in localStorage 'savedStudents' 
+and displays it in table-like format*/
+function listAllStudents(){
+	if(localStorage.savedStudents==null){
+			document.getElementById('content').innerHTML = "<h3>There are no students in the database!</h3>";
+		}
+		 else{
+			var students = JSON.parse(localStorage.getItem('savedStudents'));
+			var content = '';
+			for(i=0;i<students.length;i++){
+				content+='<p>'+students[i].name+'</p>';
+			}
+			document.getElementById('content').innerHTML = content;
+		} 
+} 
